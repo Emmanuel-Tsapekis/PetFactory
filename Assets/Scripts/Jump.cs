4188aaac4bfb64e9c512e2d714bd6ab665e3ba0f
jbenoitl <tanuki.sake@gmail.com>
2016-01-24 15:03:25 -0500

    stuff

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+	[SerializeField]
+	private Node node;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		Destroy(other.gameObject);
+	}
+	void OnCollisionEnter(Collision other){
+		Destroy(other.gameObject);
+	}
+}
