95f51cfc5156099a31dd6ece82bf8ae8e20159b1
jbenoitl <tanuki.sake@gmail.com>
2016-01-24 19:21:28 -0500

    crusher

---
+++ Assets/Scripts/Crusher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crusher : MonoBehaviour {
+
+	[SerializeField] private GameObject blood;
+	[SerializeField] private float yUp;
+	[SerializeField] private float yDown;
+	void Update()
+	{
+		if (transform.position.y >= yUp) {
+
+		}
+	}
+	void OnTriggerEnter(Collider other) {
+		Creature creature = other.GetComponent<Creature>();
+		if (creature) {
+			blood.SetActive(true);
+			Destroy (creature.gameObject);
+		}
+	}
+}
